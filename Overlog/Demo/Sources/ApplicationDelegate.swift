fd59bd9b0876ccc7cf9f2f11065d2e81da40baa7
Radosław Szeja <radoslaw.szeja@netguru.co>
2017-05-29 08:44:02 +0200

    make overlay visible

--- Overlog/Demo/Sources/ApplicationDelegate.swift
+++ Overlog/Demo/Sources/ApplicationDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Overlog
 
 /// Entry point for the application.
 @UIApplicationMain fileprivate final class ApplicationDelegate: UIResponder, UIApplicationDelegate {
@@ -15,11 +16,17 @@ import UIKit
 	/// - SeeAlso: UIApplicationDelegate.window
 	@objc(window) fileprivate lazy var window: UIWindow? = UIWindow(frame: UIScreen.main.bounds)
 
+    let overlay = Overlay(frame: UIScreen.main.bounds)
+    
 	// MARK: UIApplicationDelegate
 
 	/// - SeeAlso: UIApplicationDelegate.application(_:didFinishLaunchingWithOptions:)
 	fileprivate func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		window!.makeKeyAndVisible()
+        window!.rootViewController = UIViewController()
+        
+        overlay.window.makeKeyAndVisible()
+        
 		return true
 	}
 
