812700202b3be0b79c52e7c4bc2ad1fff310ada4
Radosław Szeja <radoslaw.szeja@netguru.co>
2017-05-30 23:07:01 +0200

    add possibility to embed child controller

--- Overlog/Sources/OverlayViewController.swift
+++ Overlog/Sources/OverlayViewController.swift
@@ -9,32 +9,69 @@ import UIKit
 
 internal final class OverlayViewController: UIViewController {
     
-    var didTapFloatingButton: ((UIButton) -> Void)? = nil
+    /// Handler of `.touchUpInside` action on `OverlayView.floatingButton`
+    internal var didTapFloatingButton: ((UIButton) -> Void)? = nil
     
-    let overlayView = OverlayView()
+    /// Handler of `.motionShake` motion event
+    internal var didPerformShakeEvent: ((UIEvent?) -> Void)? = nil
     
-    init() {
+    /// Overlay view
+    internal let overlayView = OverlayView()
+    
+    /// Overlay's child view controller
+    internal let childViewController: UIViewController
+    
+    /// Initializes overlay with its child view controller
+    ///
+    /// - Parameter childViewController: A child view controller to be embedded
+    internal init(childViewController: UIViewController) {
+        self.childViewController = childViewController
         super.init(nibName: nil, bundle: nil)
-    }
-    
-    override func loadView() {
-        view = overlayView
-    }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        overlayView.floatingButton.addTarget(self, action: #selector(didTapFloatingButton(button:)), for: .touchUpInside)
     }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
+    internal override func loadView() {
+        view = overlayView
+    }
+    
+    internal override func viewDidLoad() {
+        super.viewDidLoad()
+        overlayView.floatingButton.addTarget(
+            self,
+            action: #selector(didTapFloatingButton(button:)),
+            for: .touchUpInside
+        )
+        
+        addChildViewController(childViewController)
+        overlayView.embed(view: childViewController.view)
+        childViewController.didMove(toParentViewController: self)
+    }
+        
+    /// Overrides super method and calls `didPerformShakeEvent` closure when `.motionShake` was recevied. 
+    /// Please refer to super method documentation for further information.
+    ///
+    /// - Parameters:
+    ///   - motion: `UIEventSubtype`
+    ///   - event: `UIEvent`
+    internal override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake {
+            didPerformShakeEvent?(event)
+        }
+    }
+    
 }
 
-extension OverlayViewController {
+// MARK: - Target actions
+
+internal extension OverlayViewController {
     
-    @objc func didTapFloatingButton(button: UIButton) {
+    /// Handle action on touch up inside on `floatingButton`
+    ///
+    /// - Parameter button: floating button instance
+    @objc internal func didTapFloatingButton(button: UIButton) {
         didTapFloatingButton?(button)
     }
     
