9f64452cebf664b3f082c9a6b6a05216b83a1903
Radosław Szeja <radoslaw.szeja@netguru.co>
2017-05-30 23:07:14 +0200

    implement overlay

--- Overlog/Sources/Overlay.swift
+++ Overlog/Sources/Overlay.swift
@@ -7,19 +7,48 @@
 
 import UIKit
 
+/// An Overlay abstraction
 public final class Overlay {
     
-    public let window: UIWindow
+    /// Toggle's presentation of floating button when shake event was received.
+    public var toggleOnShakeGesture: Bool = true
     
-    fileprivate let rootViewController: OverlayViewController
+    /// Overlay's root view controller
+    private let rootViewController: OverlayViewController
     
-    public init(frame: CGRect) {
-        window = UIWindow(frame: frame)
-        rootViewController = OverlayViewController()
+    /// Overlay initializer
+    ///
+    /// - Parameters:
+    ///   - window: application's main window
+    ///   - viewController: the main window's root view controller
+    public init(window: UIWindow, rootViewController viewController: UIViewController) {
+        rootViewController = OverlayViewController(childViewController: viewController)
+        rootViewController.didPerformShakeEvent = didPerformShake(event:)
         window.rootViewController = rootViewController
-        
-        window.windowLevel = UIWindowLevelAlert
-        window.makeKeyAndVisible()
+    }
+    
+    /// Presents floating button
+    /// - Discussion:
+    ///     - This methods sets `isHidden` to `true` on `OverlayView`'s `floatingButton` only
+    public func present() {
+        rootViewController.overlayView.floatingButton.isHidden = false
+    }
+    
+    /// Hides floating button
+    /// - Discussion:
+    ///     - This methods sets `isHidden` to `false` on `OverlayView`'s `floatingButton` only
+    public func hide() {
+        rootViewController.overlayView.floatingButton.isHidden = true
+    }
+    
+    /// Shake event
+    ///
+    /// - Parameter overlayView: overlayView description
+    private func didPerformShake(event _: UIEvent?) {
+        if toggleOnShakeGesture {
+            let overlayView = rootViewController.overlayView
+            overlayView.floatingButton.isHidden = !overlayView.floatingButton.isHidden
+        }
     }
     
 }
