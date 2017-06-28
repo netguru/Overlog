aa02d95ad09187241a1fa985de75d28f36d31271
Pawel Urbanowicz <pawel.urbanowicz@netguru.co>
2017-06-28 17:59:55 +0200

    Added pan gesture handling for the floating button

--- Overlog/Sources/OverlayViewController.swift
+++ Overlog/Sources/OverlayViewController.swift
@@ -14,6 +14,9 @@ internal final class OverlayViewController: UIViewController {
     
     /// Handler of `.motionShake` motion event
     internal var didPerformShakeEvent: ((UIEvent?) -> Void)? = nil
+	
+	/// The initial center value of `OverlayView.floatingButton` during a drag gesture
+	fileprivate var initialFloatingButtonCenter: CGPoint = .zero
     
     /// Overlay view
     internal let overlayView = OverlayView()
@@ -48,6 +51,9 @@ internal final class OverlayViewController: UIViewController {
         addChildViewController(childViewController)
         overlayView.embed(view: childViewController.view)
         childViewController.didMove(toParentViewController: self)
+		
+		let panGesture = UIPanGestureRecognizer(target: self, action: #selector(didDragFloatingButton(with:)))
+		overlayView.floatingButton.addGestureRecognizer(panGesture)
     }
         
     /// Overrides super method and calls `didPerformShakeEvent` closure when `.motionShake` was recevied. 
@@ -67,12 +73,28 @@ internal final class OverlayViewController: UIViewController {
 // MARK: - Target actions
 
 internal extension OverlayViewController {
-    
+	
     /// Handle action on touch up inside on `floatingButton`
     ///
     /// - Parameter button: floating button instance
     @objc internal func didTapFloatingButton(button: UIButton) {
         didTapFloatingButton?(button)
     }
+	
+	/// Handle the pan gesture on `floatingButton`
+	///
+	/// - Parameter recognizer: pan gesture recognizer instance
+	@objc fileprivate func didDragFloatingButton(with recognizer: UIPanGestureRecognizer) {
+		if recognizer.state == .began {
+			/// Calculate the initial button center value during the gesture
+			initialFloatingButtonCenter = overlayView.floatingButton.center
+		} else if recognizer.state == .ended || recognizer.state == .changed {
+			/// Update the `overlayView.floatingButton` center value with current finger position
+			overlayView.floatingButton.center = recognizer.location(in: overlayView)
+		} else if recognizer.state == .cancelled || recognizer.state == .failed {
+			/// Bring back the initial button center if the gesture gets interrupted
+			overlayView.floatingButton.center = initialFloatingButtonCenter
+		}
+	}
     
 }
