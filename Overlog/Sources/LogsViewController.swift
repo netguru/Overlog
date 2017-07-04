7b8ec4bd3418312fc8e22184817bd273d9eaed36
Robert Abramczyk <robert.abramczyk@netguru.pl>
2017-07-04 15:34:06 +0200

    Add view controller for browsing logs

---
+++ Overlog/Sources/LogsViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LogsViewController.swift
+//  Overlog
+//
+//  Created by Robert Abramczyk on 04/07/2017.
+//  Copyright © 2017 Netguru Sp. z o.o. All rights reserved.
+//
+
+import Foundation
