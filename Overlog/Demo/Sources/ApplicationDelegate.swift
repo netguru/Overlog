3b90dc819b9cc1ccb7c6bdcedae48f644bfc58b4
Adrian Kashivskyy <adrian.kashivskyy@me.com>
2017-11-03 16:24:55 +0100

    Enable all features in Demo app

--- Overlog/Demo/Sources/ApplicationDelegate.swift
+++ Overlog/Demo/Sources/ApplicationDelegate.swift
@@ -25,7 +25,7 @@ import Overlog
         let navigationController = UINavigationController(rootViewController: ViewController())
         
         overlog = Overlog.shared
-        overlog?.configuration.features = [.url, .network]
+        overlog?.configuration.features = FeatureType.all
         overlog?.show(in: window!, rootViewController: navigationController)
 
         window?.makeKeyAndVisible()
