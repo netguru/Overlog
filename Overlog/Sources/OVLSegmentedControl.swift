0f04c2477f0a643a64cd6c1daff19c4172fb8cfe
Michał Kwiecień <michal.kwiecien@netguru.pl>
2018-01-03 13:11:45 +0100

    Segmented Control customization

--- Overlog/Sources/OVLSegmentedControl.swift
+++ Overlog/Sources/OVLSegmentedControl.swift
@@ -23,11 +23,47 @@ class OVLSegmentedControl: UISegmentedControl {
     }
     
     private func setupProperties() {
+        clipsToBounds = true
+        layer.cornerRadius = 6
         tintColor = .OVLBlue
         backgroundColor = .OVLGray
-        setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.OVLWhite], for: .normal)
-        setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.OVLWhite], for: .selected)
-        setTitleTextAttributes([NSForegroundColorAttributeName: UIColor.OVLWhite.withAlphaComponent(0.25)], for: .disabled)
+        removeBorders()
+        setTitleTextAttributes(
+            [
+            NSFontAttributeName: UIFont.OVLFont(ofSize: 14, weight: .semibold),
+            NSForegroundColorAttributeName: UIColor.OVLWhite
+            ], for: .normal)
+        
+        setTitleTextAttributes(
+            [
+                NSFontAttributeName: UIFont.OVLFont(ofSize: 14, weight: .semibold),
+                NSForegroundColorAttributeName: UIColor.OVLWhite
+            ], for: .selected)
+        
+        setTitleTextAttributes(
+            [
+                NSFontAttributeName: UIFont.OVLFont(ofSize: 14, weight: .semibold),
+                NSForegroundColorAttributeName: UIColor.OVLWhite.withAlphaComponent(0.25)
+            ], for: .disabled)
     }
-
+    
+    /// Methods that removes default borders from segmented control
+    private func removeBorders() {
+        guard let backgroundColor = backgroundColor, let tintColor = tintColor else { return }
+        setBackgroundImage(image(withColor: backgroundColor), for: .normal, barMetrics: .default)
+        setBackgroundImage(image(withColor: tintColor), for: .selected, barMetrics: .default)
+        setDividerImage(image(withColor: .clear), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
+    }
+    
+    /// Creates a 1x1 image with given color
+    private func image(withColor color: UIColor) -> UIImage? {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()
+        context?.setFillColor(color.cgColor)
+        context?.fill(rect)
+        let image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext();
+        return image
+    }
 }
